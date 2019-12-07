00347dc9afec81d1cb9e93d5688374c4e196fb53
cusmirlucian <53294551+cusmirlucian@users.noreply.github.com>
2019-12-07 19:06:22 +0200

    Revert "wadawd"
    This reverts commit b5d55073ce06030226ecdf11320e28f710e32bd0.

--- index.php
+++ index.php
@@ -14,10 +14,9 @@
 			<h1>Login</h1>
 			<input class="text" type="text" name="email" placeholder="Email"><br><br>
 			<input class="text" type="text" name="pass"	placeholder="Password"><br><br>
-			<input class="button" type="submit" value="Login"><br>
-			<p>If you don't have an account ,</p><a href="signup.php">make one</a>
-			<br><br><br>
+			<input class="button" type="submit" value="Login"><br><br><br><br>
 			</form>
 		</div>
+	
 </body>
 </html>
